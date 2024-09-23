e8a9a560c24070b5e213d55d9220e8b9ffa3de5d
v-liushuo <v-liushuo.gd@chinatelecom.cn>
2024-09-23 12:24:28 +0800

    init

--- src/main/java/com/lius/heath/task/AppleQueryTask.java
+++ src/main/java/com/lius/heath/task/AppleQueryTask.java
@@ -20,10 +20,15 @@ import org.springframework.web.client.RestTemplate;
 import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * 同步导出字段
@@ -46,11 +51,17 @@ public class AppleQueryTask {
     @Value("${mail.receive.email}")
     private String receiveEmail;
 
-    @Value("${connect.url:https://www.apple.com/hk/shop/fulfillment-messages?pl=true&mt=compact&parts.0=MYW03CH/A&searchNearby=true&store=R639}")
+    @Value("${connect.url:https://www.apple.com/hk/shop/fulfillment-messages?pl=true&mt=compact" +
+            "&parts.0=MYTN3ZA/A" +
+            "&parts.1=MYTP3ZA/A" +
+            "&parts.2=MYTQ3ZA/A" +
+            "&parts.3=MYTM3ZA/A" +
+            "&parts.4=MVX33ZP/A" +
+            "&searchNearby=true&store=R409}")
     private String connectUrl;
 
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd HH:mm:ss");
-    private static final Set<String> stores = new HashSet<>(Arrays.asList("R639", "R577"));
+    private static final Set<String> stores = new HashSet<>(Collections.emptyList());
 
     private static final AtomicInteger ERROR_COUNT = new AtomicInteger(0);
 
@@ -67,21 +78,35 @@ public class AppleQueryTask {
         JSONObject jsonObject = JSONObject.fromObject(str);
         JSONObject jsonObject1 = jsonObject.getJSONObject("body").getJSONObject("content").getJSONObject("pickupMessage");
         JSONArray jsonArray = jsonObject1.getJSONArray("stores");
-        String storeName = null;
+        Map<String, List<String>> storeHasProduct = new ConcurrentHashMap<>();
         for (Object o : jsonArray) {
             JSONObject object = (JSONObject) o;
-            if (stores.contains(object.getString("storeNumber"))) {
-                String string = object.getJSONObject("partsAvailability").getJSONObject("MQ0W3CH/A").getString("pickupDisplay");
-                if ("available".equals(string)) {
-                    storeName = object.getString("storeName");
-                    break;
+            if (stores.isEmpty() || stores.contains(object.getString("storeNumber"))) {
+                JSONObject partsAvailability = object.getJSONObject("partsAvailability");
+                Set<String> set = partsAvailability.keySet();
+                for (String key : set) {
+                    JSONObject jsonObject2 = partsAvailability.getJSONObject(key);
+                    String string = jsonObject2.getString("pickupDisplay");
+                    if ("available".equals(string)) {
+                        String storeName = object.getString("storeName");
+                        List<String> orDefault = storeHasProduct.getOrDefault(storeName, new ArrayList<>());
+                        String name = jsonObject2.getJSONObject("messageTypes").getJSONObject("compact").getString("storePickupProductTitle");
+                        orDefault.add(name);
+                        storeHasProduct.put(storeName, orDefault);
+                        break;
+                    }
                 }
             }
         }
-        if (storeName != null) {
+        if (!storeHasProduct.isEmpty()) {
+            String noticeContent = storeHasProduct.entrySet().parallelStream().map(stringListEntry -> {
+                String storeNameKey = stringListEntry.getKey();
+                String value = stringListEntry.getValue().parallelStream().collect(Collectors.joining("\n"));
+                return String.format("%s\n%s", storeNameKey, value);
+            }).collect(Collectors.joining("\n"));
             if (SystemTray.isSupported()) {
                 try {
-                    Heath1TaskApplication.displayTray(storeName);
+                    Heath1TaskApplication.displayTray(noticeContent);
                 } catch (AWTException ignored) {
                 }
             }
@@ -90,9 +115,9 @@ public class AppleQueryTask {
             mailInfoInput.setSubject("可预约通知");
             mailInfoInput.setToEmail(receiveEmail);
             mailInfoInput.setReceiveName("刘硕");
-            mailInfoInput.setMsgContent("可预约通知： " + storeName);
+            mailInfoInput.setMsgContent("可预约通知： " + noticeContent);
             logger.info("开始发送邮件通知给：" + receiveEmail);
-            noticeService.sendEmail(mailInfoInput);
+//            noticeService.sendEmail(mailInfoInput);
             logger.info("完成邮件发送通知");
             logger.info(str);
         }
